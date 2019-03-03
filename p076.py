be726112e390e0f0af45ccda895d312dce09d4fc
galustian <23199153+galustian@users.noreply.github.com>
2019-03-03 20:23:04 +0100

    add bottom-up DP approach

--- p076.py
+++ p076.py
@@ -10,11 +10,32 @@ def numWaysSum(sum_, vars_, upper_):
         ways += numWaysSum(sum_-n, vars_-1, n)
     return ways
 
+# top-down DP
 def compute():
     sum_ = 1
     for vars_ in range(2, 100):
         sum_ += numWaysSum(100, vars_, 100)
     return sum_
 
+#def print_array(array):
+#    for i in range(len(array)):
+#        print(array[i])
+
+# bottom-up DP
+def compute2():
+    ways = [[0]*101 for i in range(101)]
+    ways[0] = [0] + [1] * 100
+
+    for sum_ in range(1, 101):
+        for upper in range(1, 101):
+            if upper > sum_:
+                ways[sum_][upper] = ways[sum_][sum_]
+                continue
+            
+            ways[sum_][upper] = ways[sum_][upper-1] + ways[sum_-upper][upper]
+
+    return ways[100][100] - 1
+
 if __name__ == '__main__':
     print(compute())
+    print(compute2())
